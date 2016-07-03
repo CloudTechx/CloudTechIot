dbe5272f0c9d09cb92493a2f079f4e4eaff651d1
cloudtechx <cloudtechesx@gmail.com>
2016-07-04 00:47:23 +0800

    Check In CloudTechIot1dot5 Project

---
+++ CloudTechIot1dot5/CloudTechIot1dot5/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+
+namespace CloudTechIot1dot5
+{
+    //http://www.cnblogs.com/cloudtech
+    //cloudtechesx@gmail.com
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get
+            {
+                return _currentTime;
+            }
+
+            set
+            {
+                _currentTime = value;
+                OnProperityChanged("CurrentTime");
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+            CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public void OnProperityChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(propertyName, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
